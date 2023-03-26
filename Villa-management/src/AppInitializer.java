b7867d3c70638b141836c1e5bc16072796dbb439
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-26 23:15:33 +0530

    Add AppInitializer

---
+++ Villa-management/src/AppInitializer.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AdminDashBoardForm.fxml"))));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+
+
+    }
+}
