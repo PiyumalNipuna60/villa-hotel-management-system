c742a189585d00d220be72be9ccdb24d576067d6
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-04-01 23:08:30 +0530

    Add EmployeeSchedule class

---
+++ Villa-management/src/model/EmployeeSchedule.java
@@ -0,0 +1,72 @@
+package model;
+
+public class EmployeeSchedule {
+
+    private String scheduleId;
+    private String time;
+    private String date;
+    private String shift;
+    private String empId;
+
+    public EmployeeSchedule() {
+    }
+
+    public EmployeeSchedule(String scheduleId, String time, String date, String shift, String empId) {
+        this.scheduleId = scheduleId;
+        this.time = time;
+        this.date = date;
+        this.shift = shift;
+        this.empId = empId;
+    }
+
+    public String getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeSchedule{" +
+                "scheduleId='" + scheduleId + '\'' +
+                ", time='" + time + '\'' +
+                ", date='" + date + '\'' +
+                ", shift='" + shift + '\'' +
+                ", empId='" + empId + '\'' +
+                '}';
+    }
+}
