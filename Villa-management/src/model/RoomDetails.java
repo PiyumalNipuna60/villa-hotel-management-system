7cb63af204cf42ea75fbc13a2ad7d752f2941e57
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-04-01 23:08:54 +0530

    Add RoomDetails class

---
+++ Villa-management/src/model/RoomDetails.java
@@ -0,0 +1,50 @@
+package model;
+
+public class RoomDetails {
+
+    private String roomId;
+    private String empId;
+    private String availabele;
+
+    public RoomDetails() {
+    }
+
+    public RoomDetails(String roomId, String empId, String availabele) {
+        this.roomId = roomId;
+        this.empId = empId;
+        this.availabele = availabele;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getAvailabele() {
+        return availabele;
+    }
+
+    public void setAvailabele(String availabele) {
+        this.availabele = availabele;
+    }
+
+    @Override
+    public String toString() {
+        return "RoomDetails{" +
+                "roomId='" + roomId + '\'' +
+                ", empId='" + empId + '\'' +
+                ", availabele='" + availabele + '\'' +
+                '}';
+    }
+}
