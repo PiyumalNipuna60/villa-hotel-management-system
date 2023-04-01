5f1bbd70322b805cbcade9892575be455b6ff9a0
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-04-01 23:08:43 +0530

    Add Room class

---
+++ Villa-management/src/model/Room.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Room {
+
+    private String roomId;
+    private String type;
+    private String description;
+    private String price;
+
+    public Room() {
+    }
+
+    public Room(String roomId, String type, String description, String price) {
+        this.roomId = roomId;
+        this.type = type;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomId='" + roomId + '\'' +
+                ", type='" + type + '\'' +
+                ", description='" + description + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+}
