deeacdc0a40b10a3191e255f7cfdc05532fb7d1d
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-04-03 23:51:59 +0530

    Add Class

--- Villa-management/src/controller/CustomerFormController.java
+++ Villa-management/src/controller/CustomerFormController.java
@@ -27,282 +27,114 @@ import java.time.format.DateTimeFormatter;
 public class CustomerFormController {
 
     public DatePicker txtDob;
+    @FXML
     private ComboBox cmbSafaryid;
+    @FXML
     private ComboBox cmbSafaryType;
 
+    @FXML
     private TableColumn colAddress;
 
+    @FXML
     private TableColumn colBirthday;
 
+    @FXML
     private TableColumn colContact;
 
+    @FXML
     private TableColumn colGender;
 
+    @FXML
     private TableColumn colId;
 
+    @FXML
     private TableColumn colName;
 
+    @FXML
     private TableColumn colNic;
 
+    @FXML
     private TableColumn colSalary;
 
+    @FXML
     private Label lblDate;
 
+    @FXML
     private RadioButton rbnFemale;
 
+    @FXML
     private RadioButton rbnMale;
 
+    @FXML
     private ToggleGroup sexuaol;
 
+    @FXML
     private TableView<CustomerTm> tblCustomer;
 
+    @FXML
     private TextField txtContact;
 
+    @FXML
     private TextField txtCusAddress;
 
+    @FXML
     private TextField txtCusId;
 
+    @FXML
     private TextField txtCusName;
 
+    @FXML
     private TextField txtCusNic;
 
     public void initialize() {
-        loadComboBox();
 
-        colId.setCellValueFactory(new PropertyValueFactory("cusId"));
-        colName.setCellValueFactory(new PropertyValueFactory("name"));
-        colAddress.setCellValueFactory(new PropertyValueFactory("address"));
-        colBirthday.setCellValueFactory(new PropertyValueFactory("dob"));
-        colNic.setCellValueFactory(new PropertyValueFactory("nic"));
-        colContact.setCellValueFactory(new PropertyValueFactory("contact"));
-        colGender.setCellValueFactory(new PropertyValueFactory("sex"));
-        colSalary.setCellValueFactory(new PropertyValueFactory("safaryId"));
 
-        LoadAllCustomer();
-        generateRealTime();
+    }
+
+    private void setCustomerFields(Object newValue) {
 
     }
 
     private void LoadAllCustomer() {
-        tblCustomer.getItems().clear();
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement pstm = connection.prepareStatement("select*from customer");
-            ResultSet rst = pstm.executeQuery();
-            while (rst.next()) {
-                tblCustomer.getItems().add(new CustomerTm(
-                        rst.getString("cusId"),
-                        rst.getString("name"),
-                        rst.getString("address"),
-                        rst.getString("dob"),
-                        rst.getString("Nic"),
-                        rst.getString("Contact"),
-                        rst.getString("sex"),
-                        rst.getString("safaryId")));
-            }
 
-
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     private void loadComboBox() {
-        ObservableList obList = FXCollections.observableArrayList();
-        obList.add("S001");
-        obList.add("S002");
-        cmbSafaryid.setItems(obList);
 
-        ObservableList obList1 = FXCollections.observableArrayList();
-        obList1.add("Yala");
-        obList1.add("Beach");
-        cmbSafaryType.setItems(obList1);
     }
 
+    @FXML
     void btnDeleteOnAction(ActionEvent event) {
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE cusId=?");
-            pstm.setString(1, txtCusId.getText());
-            boolean b = pstm.executeUpdate() > 0;
-            if (b){
-                new Alert(Alert.AlertType.INFORMATION, "Customer " + txtCusId.getText() + " Delete..!").show();
-                LoadAllCustomer();
-                btnClearOnAction();
-            }else {
-                new Alert(Alert.AlertType.ERROR, "Something Wrong..!").show();
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+
 
     }
 
+    @FXML
     void btnSaveOnAction(ActionEvent event) {
-        String Id = txtCusId.getText();
-        String name = txtCusName.getText();
-        String address = txtCusAddress.getText();
-        LocalDate dob = txtDob.getValue();
-        String nic = txtCusNic.getText();
-        String contact = txtContact.getText();
-        String gender;
-        Object safaryId = cmbSafaryid.getValue();
-        Object safaryType = cmbSafaryType.getValue();
 
-        if (rbnMale.isSelected()) {
-            gender = "Male";
-        } else {
-            gender = "Femal";
-        }
-
-
-        try {
-            if (!existCustomer(Id)){
-                Connection connection = DBConnection.getInstance().getConnection();
-                PreparedStatement pstm = connection.prepareStatement("Insert into Customer Values (?,?,?,?,?,?,?,?,?)");
-                pstm.setString(1, Id);
-                pstm.setString(2, name);
-                pstm.setString(3, address);
-                pstm.setString(4, String.valueOf(dob));
-                pstm.setString(5, nic);
-                pstm.setString(6, contact);
-                pstm.setString(7, gender);
-                pstm.setString(8, String.valueOf(safaryId));
-                pstm.setString(9, String.valueOf(safaryType));
-                boolean x = pstm.executeUpdate() > 0;
-
-                if (x) {
-                    new Alert(Alert.AlertType.INFORMATION, "Customer " + Id + " Saved..!").show();
-                    LoadAllCustomer();
-                    btnClearOnAction();
-                } else {
-                    new Alert(Alert.AlertType.ERROR, "Something Wrong..!").show();
-                }
-            }else {
-                new Alert(Alert.AlertType.ERROR, "Customer Id Already Add..!").show();
-            }
-
-
-        } catch (SQLException | ClassNotFoundException e) {
-
-        }
 
     }
 
+    @FXML
     void btnSearchOnAction() {
-        String id = txtCusId.getText();
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement pstm = connection.prepareStatement("select*from customer where cusId=?");
-            pstm.setString(1, id);
-            ResultSet rst = pstm.executeQuery();
 
-            if (rst.next()) {
-                txtCusId.setText(rst.getString(1));
-                txtCusName.setText(rst.getString(2));
-                txtCusAddress.setText(rst.getString(3));
-                System.out.println(rst.getString(4));
-                txtDob.setValue(LocalDate.parse(rst.getString(4)));
-                txtCusNic.setText(rst.getString(5));
-                txtContact.setText(rst.getString(6));
-                String value = rst.getString(7);
-                System.out.println(value);
-                if (value.equals("Male")) {
-                    rbnMale.setSelected(true);
-                } else {
-                    rbnFemale.setSelected(true);
-                }
-
-                cmbSafaryid.setValue(rst.getObject(8));
-                cmbSafaryType.setValue(rst.getObject(9));
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
     }
 
+    @FXML
     void btnUpdateOnAction(ActionEvent event) {
-        String Id = txtCusId.getText();
-        String name = txtCusName.getText();
-        String address = txtCusAddress.getText();
-        LocalDate dob = txtDob.getValue();
-        String nic = txtCusNic.getText();
-        String contact = txtContact.getText();
-        String gender;
-        Object safaryId = cmbSafaryid.getValue();
-        Object safaryType = cmbSafaryType.getValue();
 
-        if (rbnMale.isSelected()) {
-            gender = "Male";
-        } else {
-            gender = "Femal";
-        }
-
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=?,dob=?,nic=?,contact=?,sex=?,safaryId=?,type=? WHERE cusId=?");
-            pstm.setString(1, name);
-            pstm.setString(2, address);
-            pstm.setString(3, String.valueOf(dob));
-            pstm.setString(4, nic);
-            pstm.setString(5, contact);
-            pstm.setString(6, gender);
-            pstm.setString(7, String.valueOf(safaryId));
-            pstm.setString(8, String.valueOf(safaryType));
-            pstm.setString(9, Id);
-            boolean x = pstm.executeUpdate() > 0;
-
-            if (x) {
-                new Alert(Alert.AlertType.INFORMATION, "Customer " + Id + " Update..!").show();
-                LoadAllCustomer();
-                btnClearOnAction();
-            } else {
-                new Alert(Alert.AlertType.ERROR, "Something Wrong..!").show();
-            }
-
-
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public void SearchOnKeyPress(KeyEvent keyEvent) {
-        btnSearchOnAction();
-    }
 
-    public void btnClearOnAction() {
-        txtCusId.clear();
-        txtCusName.clear();
-        txtCusAddress.clear();
-        txtCusNic.clear();
-        txtContact.clear();
-        txtDob.setValue(LocalDate.parse("2000-01-01"));
-        cmbSafaryid.getItems().clear();
-        cmbSafaryType.getItems().clear();
-        rbnMale.setSelected(false);
-        rbnFemale.setSelected(false);
 
-    }
+
 
 
     //----------------------Set Date & time--------------------------------------------------------------
-    private void generateRealTime() {
-        //lblDate.setText(LocalDate.now().toString());
-        Timeline timeline = new Timeline(new KeyFrame(Duration.ZERO, e -> {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
-            lblDate.setText(LocalDateTime.now().format(formatter));
-        }), new KeyFrame(Duration.seconds(1)));
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
-    }
 
 
     //----------------------existCustomer--------------------------------------------------------------
-    boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("SELECT cusId FROM Customer WHERE cusId=?");
-        pstm.setString(1, id);
-        return pstm.executeQuery().next();
-    }
+
 }
