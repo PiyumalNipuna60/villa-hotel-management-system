0bb5abac90d7d066bae7516662619092f16c5899
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-04-06 23:49:35 +0530

    Add update

---
+++ Villa-management/src/dto/BillDTO.java
@@ -0,0 +1,24 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+public class BillDTO {
+    private String cusId;
+    private String Name;
+    private String contact;
+    private String safaryType;
+    private  String roomId;
+    private String roomType;
+    private String roomPrice;
+    private String payment;
+    private String bill;
+    private String cash;
+    private String balance;
+}
